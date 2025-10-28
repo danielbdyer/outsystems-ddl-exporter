c73d1cb15d4c0b650b0c2933c0b312c386b24f5f
Daniel Dyer <danielbdyer@gmail.com>
2025-10-28 10:23:10 -0700

    Normalize boolean defaults for BIT columns

--- tests/Osm.Smo.Tests/SmoColumnBuilderTests.cs
+++ tests/Osm.Smo.Tests/SmoColumnBuilderTests.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.SqlServer.Management.Smo;
 using Osm.Smo;
+using Osm.Validation.Tightening;
 using Xunit;
 
 namespace Osm.Smo.Tests;
@@ -44,5 +46,47 @@ public class SmoColumnBuilderTests
         var cityColumns = SmoColumnBuilder.BuildColumns(cityContext, decisions, profileDefaults, TypeMappingPolicy.Default, contexts);
         var isActive = cityColumns.Single(c => c.LogicalName.Equals("IsActive", StringComparison.Ordinal));
         Assert.Equal("((1))", isActive.DefaultExpression);
+    }
+
+    [Fact]
+    public void BuildColumns_normalizes_boolean_word_default_to_bit_literal()
+    {
+        var (model, decisions, _) = SmoTestHelper.LoadEdgeCaseArtifacts();
+        var moduleIndex = Enumerable.Range(0, model.Modules.Length)
+            .First(i => model.Modules[i].Entities.Any(entity =>
+                entity.LogicalName.Value.Equals("City", StringComparison.Ordinal)));
+        var module = model.Modules[moduleIndex];
+        var entityIndex = Enumerable.Range(0, module.Entities.Length)
+            .First(i => module.Entities[i].LogicalName.Value.Equals("City", StringComparison.Ordinal));
+        var cityEntity = module.Entities[entityIndex];
+        var attributeIndex = Enumerable.Range(0, cityEntity.Attributes.Length)
+            .First(i => cityEntity.Attributes[i].LogicalName.Value.Equals("IsActive", StringComparison.Ordinal));
+        var isActiveAttribute = cityEntity.Attributes[attributeIndex];
+
+        var modifiedAttribute = isActiveAttribute with
+        {
+            DefaultValue = "True",
+            OnDisk = isActiveAttribute.OnDisk with { DefaultDefinition = null, DefaultConstraint = null },
+        };
+
+        var modifiedAttributes = cityEntity.Attributes.SetItem(attributeIndex, modifiedAttribute);
+        var modifiedCityEntity = cityEntity with { Attributes = modifiedAttributes };
+        var modifiedEntities = module.Entities.SetItem(entityIndex, modifiedCityEntity);
+        var modifiedModule = module with { Entities = modifiedEntities };
+        var modifiedModules = model.Modules.SetItem(moduleIndex, modifiedModule);
+        var modifiedModel = model with { Modules = modifiedModules };
+
+        var contexts = SmoModelFactory.BuildEntityContexts(modifiedModel, supplementalEntities: null);
+        var cityContext = contexts.GetContext(modifiedCityEntity);
+        var profileDefaults = ImmutableDictionary<ColumnCoordinate, string>.Empty;
+        var columns = SmoColumnBuilder.BuildColumns(
+            cityContext,
+            decisions,
+            profileDefaults,
+            TypeMappingPolicy.Default,
+            contexts);
+
+        var isActive = columns.Single(c => c.LogicalName.Equals("IsActive", StringComparison.Ordinal));
+        Assert.Equal("(1)", isActive.DefaultExpression);
     }
 }
